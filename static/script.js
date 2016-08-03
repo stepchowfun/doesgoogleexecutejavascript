cd38c79e72fd4befc68be6b820fc062d0ecd4bcc
Stephan Boyer <stephan@stephanboyer.com>
2016-08-02 21:49:53 -0700

    Add an explanation of the experiment

--- static/script.js
+++ static/script.js
@@ -3,15 +3,13 @@ document.addEventListener('DOMContentLoaded', function() {
     '<h1>Yes,</h1>' +
     '<h2>' +
       'Google executes JavaScript, even if the script is fetched from ' +
-      'the network. However, Google does not execute AJAX requests.' +
+      'the network. However, Google does not make AJAX requests.' +
     '</h2>';
 
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function() {
-      if (xmlhttp.readyState === XMLHttpRequest.DONE) {
-        if (xmlhttp.status === 200) {
-          document.getElementById('content').innerHTML = JSON.parse(xmlhttp.responseText).message;
-        }
+      if (xmlhttp.readyState === XMLHttpRequest.DONE && xmlhttp.status === 200) {
+        document.getElementById('content').innerHTML = JSON.parse(xmlhttp.responseText).message;
       }
     };
     xmlhttp.open("GET", "/ajax", true);
